0652f9931373e30a42ef5e0a4cbb6a69679b1615
vishal shivakumar <23b160@psgitech.ac.in>
2025-07-24 14:44:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI,Request,UploadFile,File,HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import os
 import subprocess
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel 
+from typing import Annotated
+import aiofiles 
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")  #  Specify your templates directory
 
@@ -21,20 +24,37 @@ class Item(BaseModel):
     name: str
     description: str | None = None
 
+
 @app.get("/getmethod")
 async def read_root():
     return {"message": "Hello from FastAPI!"}
 
-# @app.get("/", response_class=HTMLResponse)
-# async def read_root(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post('/upload_file')
+async def upload_file(file: Annotated[UploadFile, File]):
+    print("inside upload file")
+
+    try:
+         destination_path = r"C:\Users\50054\Desktop\CV_Analyser\data"
+         destination_path = os.path.join(destination_path, file.filename)
+         print(destination_path)
+         async with aiofiles.open(destination_path, "wb") as out_file:
+             content = await file.read()  
+             await out_file.write(content)
+         print(f"File '{file.filename}' successfully saved to '{destination_path}'")
+         return {"message": f"File '{file.filename}' uploaded successfully!", "location": destination_path}
+    except Exception as e:
+        print(f"ERROR: During file upload for '{file.filename}': {e}")
+        raise HTTPException(status_code=500, detail=f"Failed to upload file. Server error: {e}")
+
+    
 
 
-@app.post("/run-python-script",methods=['POST'])
+@app.post("/run_python_script")
 async def run_script():
     print("inside run_script")
     try:
-        result = subprocess.run(["python", "view.py", "1"], capture_output=True, text=True, check=True)
+        result = subprocess.run(["python", r"C:\Users\50054\Desktop\CV_Analyser\backend\view.py", "1"], capture_output=True, text=True, check=True)
         return {"message": f"Python script executed successfully! Output: {result.stdout}"}
     except subprocess.CalledProcessError as e:
         return {"message": f"Error running Python script: {e.stderr}"}
